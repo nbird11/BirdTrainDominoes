4ea803da3a52112bab117c9f1b064a1d0337daff
Nathan Bird <nathanbirdka@gmail.com>
2023-01-01 16:14:38 -0700

    More work on Board and Branch classes

--- bird_train.py
+++ bird_train.py
@@ -8,6 +8,7 @@ import random
 AMMT_PLAYERS = 4
 DEAL_AMMT = 15
 DOUBLE_TWELVES_VALUE = 12
+AMMT_BRANCHES = 8
 
 class Domino:
     """A tile that has a certain amount of dots (pips) on both ends."""
@@ -51,32 +52,36 @@ class Boneyard:
         """Representation of the `Boneyard` object, including all dominoes
         still left in the draw pile.
         """
-
         self._update()
+        # rep: str = "Boneyard("
+        # rep += "\n\t\tdraw_pile=["
+        # for i, domino in enumerate(self.draw_pile):
+        #     # Last Domino but not first column
+        #     if i == len(self.draw_pile)-1 and i % 3 != 0:
+        #         rep += f"{domino}"
+        #     # Last Domino and first column
+        #     elif i == len(self.draw_pile)-1:
+        #         rep += f"\n\t\t\t{domino}"
+        #     # First column
+        #     elif i % 3 == 0:
+        #         col_1: Domino = domino
+        #         rep += f"\n\t\t\t{col_1},\t"
+        #     # Second column
+        #     elif i % 3 == 1:
+        #         col_2: Domino = domino
+        #         rep += f"{col_2},\t"
+        #     # Third column
+        #     elif i % 3 == 2:
+        #         col_3: Domino = domino
+        #         rep += f"{col_3}"
+        # rep += f"],"
+        # rep += f"\n\t\tcount_tiles={self.tile_count},"
+        # rep += f"\n\t\tround_num={self.round_num}\n\t)"
+
         rep: str = "Boneyard("
-        rep += "\n\t\tdraw_pile=["
-        for i, domino in enumerate(self.draw_pile):
-            # Last Domino but not first column
-            if i == len(self.draw_pile)-1 and i % 3 != 0:
-                rep += f"{domino}"
-            # Last Domino and first column
-            elif i == len(self.draw_pile)-1:
-                rep += f"\n\t\t\t{domino}"
-            # First column
-            elif i % 3 == 0:
-                col_1: Domino = domino
-                rep += f"\n\t\t\t{col_1},\t"
-            # Second column
-            elif i % 3 == 1:
-                col_2: Domino = domino
-                rep += f"{col_2},\t"
-            # Third column
-            elif i % 3 == 2:
-                col_3: Domino = domino
-                rep += f"{col_3}"
-        rep += f"],"
-        rep += f"\n\t\tcount_tiles={self.tile_count},"
-        rep += f"\n\t\tround_num={self.round_num}\n\t)"
+        rep += f"draw_pile={self.draw_pile}, "
+        rep += f"tile_count={self.tile_count}, "
+        rep += f"round_num={self.round_num})"
         return rep
 
     def _update(self) -> None:
@@ -93,29 +98,64 @@ class Player:
     def __init__(self, ai: bool = False) -> None:
         """Constructs a `Player`."""
         self.ai: bool = ai
-        self.hand: set = set()
+        self.hand: set[Domino] = set()
 
     def __repr__(self) -> str:
         """Representation of an instance of `Player`."""
         rep: str = "Player("
-        rep += f"\n\t\tai={self.ai}, "
-        rep += f"\n\t\thand={self.hand}\n\t)"
+        rep += f"ai={self.ai}, "
+        rep += f"hand={self.hand})"
         return rep
     
-    def _draw(self, boneyard: Boneyard) -> None:
+    def draw(self, boneyard: Boneyard) -> None:
         """Takes the last domino in the passed boneyard and puts it in the
         player's hand.
         """
         self.hand.add(boneyard.draw_pile.pop())
 
 
+class Branch:
+    """TODO A branch of the play area."""
+    def __init__(self, player: Player) -> None:
+        """Constructs a `Branch` object."""
+        self.player: Player = player
+        # Assign whether the branch can be played on by anyone.
+        self.train_on: bool = None
+        if self.player:
+            self.train_on = True
+        else:
+            self.train_on = False
+        self.train: list[Domino] = []
+
+    def __repr__(self) -> str:
+        rep: str = "Branch("
+        rep += f"player={self.player}, "
+        rep += f"train_on={self.train_on}, "
+        rep += f"train={self.train})"
+        return rep
+
+
 class Board:
     """TODO The board that the game is played on."""
-    def __init__(self) -> None:
-        """TODO Constructs a `Board` object for the whole game."""
-    
-    def _round_reset(self) -> None:
-        """TODO Resets the board after each round."""
+    def __init__(self, num_round: int, players: list[Player]) -> None:
+        """Constructs a `Board` object for the round."""
+        self.branches: list[Branch] = []
+        self.round_num: int = num_round
+        self.middle_tile: Domino = Domino(self.round_num, self.round_num)
+        self.players: list[Player] = players
+
+        for player in self.players:
+            self.branches.append(Branch(player))
+        
+        print(f"board.branches[0].player={self.branches[0].player}")
+
+    def __repr__(self) -> str:
+        rep: str = "Board("
+        rep += f"branches={self.branches}, "
+        rep += f"round_num={self.round_num}, "
+        rep += f"middle_tile={self.middle_tile}, "
+        rep += f"players={self.players})"
+        return rep
 
 
 class Game:
@@ -132,32 +172,34 @@ class Game:
                 ammt_players_real -= 1
             else:
                 self.players.append(Player(ai=True))
-        self.turn_i: int = -1
+        self.turn_i: int = -1       # Starts at -1 because `_update_turn` increments it
         self.round_num: int = 12
         self.boneyard: Boneyard = None
+        self.board: Board = None
 
     def __repr__(self) -> None:
         """Representation of all current game info."""
-        rep: str = "\nGame("
-        rep += f"\n\tammt_players={self.ammt_players},"
-        rep += f"\n\tplayers={self.players},"
-        rep += f"\n\tturn_i={self.turn_i},"
-        rep += f"\n\tround_num={self.round_num},"
-        rep += f"\n\tboneyard={self.boneyard}\n)"
+        rep: str = "Game("
+        rep += f"ammt_players={self.ammt_players},"
+        rep += f"players={self.players},"
+        rep += f"turn_i={self.turn_i},"
+        rep += f"round_num={self.round_num},"
+        rep += f"boneyard={self.boneyard})"
         return rep
 
     def gameloop(self) -> None:
         """Handle the main gameloop."""
         while self.round_num >= 0:
-            self._roundloop(self.round_num)
+            self._roundloop()
+            print(self)
             return              # For testing the round loop for one round
             self.round_num -= 1
 
-    def _roundloop(self, num_round: int) -> None:
+    def _roundloop(self) -> None:
         """Handle the loop for each individual round."""
-        self._round_init(num_round)
+        self._round_init()
         self._update_turn()
-
+        
         # show_possible_moves()             # TODO
         # if no_possible_moves              # TODO
             # draw_tile()                   # TODO
@@ -167,12 +209,11 @@ class Game:
         # else                              # TODO
             # choose_move() ~               # TODO
 
-    def _round_init(self, num_round: int) -> None:
+    def _round_init(self) -> None:
         """Starts the round."""
-        
-        self.round_num = num_round
         self.boneyard = Boneyard(self.round_num)
         self._deal()
+        self.board = Board(self.round_num, self.players)
 
     def _deal(self) -> None:
         """Deals the tiles from the boneyard. Is called at the beginning of
@@ -180,7 +221,7 @@ class Game:
         """
         for _ in range(DEAL_AMMT):
             for player in self.players:
-                player._draw(self.boneyard)
+                player.draw(self.boneyard)
 
     def _update_turn(self) -> None:
         """Updates the turn cycle to the next player's index in `players` list"""
