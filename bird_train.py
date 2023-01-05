5f9d41bebcf656d5aa028795459b3f95ca662e63
Nathan Bird <nathanbirdka@gmail.com>
2023-01-05 08:57:14 -0700

    Functionality Progress
    Added basis for `initial_train` method logic for real players to pick
    the dominoes in their initial train.
    
    Tested out some ascii art for displaying the dominoes in a text format,
    and added functionality for displaying them in the `Domino` class, to be
    used with a (future) separate object `Cursor` to draw (print) to the
    screen.

--- bird_train.py
+++ bird_train.py
@@ -22,16 +22,247 @@ class Domino:
         end1: str
         end2: str
         if self.end1_pips < 10:
-            end1 = f"0{self.end1_pips}"
+            end1 = f" {self.end1_pips}"
         else:
             end1 = f"{self.end1_pips}"
         if self.end2_pips < 10:
-            end2 = f"0{self.end2_pips}"
+            end2 = f"{self.end2_pips} "
         else:
             end2 = f"{self.end2_pips}"
         
-        rep: str = f"{end1}|{end2}"
+        # rep: str = f"[{end1}|{end2}]"
+        rep: str = f"[{self.end1_pips}|{self.end2_pips}]"
         return rep
+
+    def add_ascii_to_cursor(self, rows: list[str] = ['', '', '', '', '', '', '', '', '']):
+        four_top = 'O OOO'
+        four_across = 'OO OO'
+        four_btm = 'OOO O'
+        three_across = 'O O O'
+        two_across = 'O   O'
+        one_top = 'O    '
+        one_mid = '  O  '
+        one_btm = '    O'
+        blank_across = '     '
+        
+        # Draw vertical.
+        if self.end1_pips == self.end2_pips:
+            # Top, middle, bottom rows
+            rows[0] += '╭─────╮'
+            rows[4] += '├─────┤'
+            rows[8] += '╰─────╯'
+
+            # Domino values
+            if self.end1_pips == 12:
+                rows[1] += f'│{four_top}│'
+                rows[2] += f'│{four_across}│'
+                rows[3] += f'│{four_btm}│'
+                rows[5] += f'│{four_top}│'
+                rows[6] += f'│{four_across}│'
+                rows[7] += f'│{four_btm}│'
+            elif self.end1_pips == 11:
+                rows[1] += f'│{four_top}│'
+                rows[2] += f'│{three_across}│'
+                rows[3] += f'│{four_btm}│'
+                rows[5] += f'│{four_top}│'
+                rows[6] += f'│{three_across}│'
+                rows[7] += f'│{four_btm}│'
+            elif self.end1_pips == 10:
+                rows[1] += f'│{four_across}│'
+                rows[2] += f'│{two_across}│'
+                rows[3] += f'│{four_across}│'
+                rows[5] += f'│{four_across}│'
+                rows[6] += f'│{two_across}│'
+                rows[7] += f'│{four_across}│'
+            elif self.end1_pips == 9:
+                rows[1] += f'│{three_across}│'
+                rows[2] += f'│{three_across}│'
+                rows[3] += f'│{three_across}│'
+                rows[5] += f'│{three_across}│'
+                rows[6] += f'│{three_across}│'
+                rows[7] += f'│{three_across}│'
+            elif self.end1_pips == 8:
+                rows[1] += f'│{three_across}│'
+                rows[2] += f'│{two_across}│'
+                rows[3] += f'│{three_across}│'
+                rows[5] += f'│{three_across}│'
+                rows[6] += f'│{two_across}│'
+                rows[7] += f'│{three_across}│'
+            elif self.end1_pips == 7:
+                rows[1] += f'│{two_across}│'
+                rows[2] += f'│{three_across}│'
+                rows[3] += f'│{two_across}│'
+                rows[5] += f'│{two_across}│'
+                rows[6] += f'│{three_across}│'
+                rows[7] += f'│{two_across}│'
+            elif self.end1_pips == 6:
+                rows[1] += f'│{two_across}│'
+                rows[2] += f'│{two_across}│'
+                rows[3] += f'│{two_across}│'
+                rows[5] += f'│{two_across}│'
+                rows[6] += f'│{two_across}│'
+                rows[7] += f'│{two_across}│'
+            elif self.end1_pips == 5:
+                rows[1] += f'│{two_across}│'
+                rows[2] += f'│{one_mid}│'
+                rows[3] += f'│{two_across}│'
+                rows[5] += f'│{two_across}│'
+                rows[6] += f'│{one_mid}│'
+                rows[7] += f'│{two_across}│'
+            elif self.end1_pips == 4:
+                rows[1] += f'│{two_across}│'
+                rows[2] += f'│{blank_across}│'
+                rows[3] += f'│{two_across}│'
+                rows[5] += f'│{two_across}│'
+                rows[6] += f'│{blank_across}│'
+                rows[7] += f'│{two_across}│'
+            elif self.end1_pips == 3:
+                rows[1] += f'│{one_top}│'
+                rows[2] += f'│{one_mid}│'
+                rows[3] += f'│{one_btm}│'
+                rows[5] += f'│{one_top}│'
+                rows[6] += f'│{one_mid}│'
+                rows[7] += f'│{one_btm}│'
+            elif self.end1_pips == 2:
+                rows[1] += f'│{one_top}│'
+                rows[2] += f'│{blank_across}│'
+                rows[3] += f'│{one_btm}│'
+                rows[5] += f'│{one_top}│'
+                rows[6] += f'│{blank_across}│'
+                rows[7] += f'│{one_btm}│'
+            elif self.end1_pips == 1:
+                rows[1] += f'│{blank_across}│'
+                rows[2] += f'│{one_mid}│'
+                rows[3] += f'│{blank_across}│'
+                rows[5] += f'│{blank_across}│'
+                rows[6] += f'│{one_mid}│'
+                rows[7] += f'│{blank_across}│'
+            elif self.end1_pips == 0:
+                rows[1] += f'│{blank_across}│'
+                rows[2] += f'│{blank_across}│'
+                rows[3] += f'│{blank_across}│'
+                rows[5] += f'│{blank_across}│'
+                rows[6] += f'│{blank_across}│'
+                rows[7] += f'│{blank_across}│'
+        # Draw horizontal.
+        else:
+            # Top and bottom rows
+            rows[0] += '             '
+            rows[1] += '             '
+            rows[2] += '╭─────┬─────╮'
+            rows[6] += '╰─────┴─────╯'
+            rows[7] += '             '
+            rows[8] += '             '
+
+            # Domino values end1
+            if self.end1_pips == 12:
+                rows[3] += f'|{four_top}|'
+                rows[4] += f'|{four_across}|'
+                rows[5] += f'|{four_btm}|'
+            elif self.end1_pips == 11:
+                rows[3] += f'│{four_top}│'
+                rows[4] += f'│{three_across}│'
+                rows[5] += f'│{four_btm}│'
+            elif self.end1_pips == 10:
+                rows[3] += f'│{four_across}│'
+                rows[4] += f'│{two_across}│'
+                rows[5] += f'│{four_across}│'
+            elif self.end1_pips == 9:
+                rows[3] += f'│{three_across}│'
+                rows[4] += f'│{three_across}│'
+                rows[5] += f'│{three_across}│'
+            elif self.end1_pips == 8:
+                rows[3] += f'│{three_across}│'
+                rows[4] += f'│{two_across}│'
+                rows[5] += f'│{three_across}│'
+            elif self.end1_pips == 7:
+                rows[3] += f'│{two_across}│'
+                rows[4] += f'│{three_across}│'
+                rows[5] += f'│{two_across}│'
+            elif self.end1_pips == 6:
+                rows[3] += f'│{two_across}│'
+                rows[4] += f'│{two_across}│'
+                rows[5] += f'│{two_across}│'
+            elif self.end1_pips == 5:
+                rows[3] += f'│{two_across}│'
+                rows[4] += f'│{one_mid}│'
+                rows[5] += f'│{two_across}│'
+            elif self.end1_pips == 4:
+                rows[3] += f'│{two_across}│'
+                rows[4] += f'│{blank_across}│'
+                rows[5] += f'│{two_across}│'
+            elif self.end1_pips == 3:
+                rows[3] += f'│{one_top}│'
+                rows[4] += f'│{one_mid}│'
+                rows[5] += f'│{one_btm}│'
+            elif self.end1_pips == 2:
+                rows[3] += f'│{one_top}│'
+                rows[4] += f'│{blank_across}│'
+                rows[5] += f'│{one_btm}│'
+            elif self.end1_pips == 1:
+                rows[3] += f'│{blank_across}│'
+                rows[4] += f'│{one_mid}│'
+                rows[5] += f'│{blank_across}│'
+            elif self.end1_pips == 0:
+                rows[3] += f'│{blank_across}│'
+                rows[4] += f'│{blank_across}│'
+                rows[5] += f'│{blank_across}│'
+
+            # Domino values end2
+            if self.end2_pips == 12:
+                rows[3] += f'{four_top}|'
+                rows[4] += f'{four_across}|'
+                rows[5] += f'{four_btm}|'
+            elif self.end2_pips == 11:
+                rows[3] += f'{four_top}│'
+                rows[4] += f'{three_across}│'
+                rows[5] += f'{four_btm}│'
+            elif self.end2_pips == 10:
+                rows[3] += f'{four_across}│'
+                rows[4] += f'{two_across}│'
+                rows[5] += f'{four_across}│'
+            elif self.end2_pips == 9:
+                rows[3] += f'{three_across}│'
+                rows[4] += f'{three_across}│'
+                rows[5] += f'{three_across}│'
+            elif self.end2_pips == 8:
+                rows[3] += f'{three_across}│'
+                rows[4] += f'{two_across}│'
+                rows[5] += f'{three_across}│'
+            elif self.end2_pips == 7:
+                rows[3] += f'{two_across}│'
+                rows[4] += f'{three_across}│'
+                rows[5] += f'{two_across}│'
+            elif self.end2_pips == 6:
+                rows[3] += f'{two_across}│'
+                rows[4] += f'{two_across}│'
+                rows[5] += f'{two_across}│'
+            elif self.end2_pips == 5:
+                rows[3] += f'{two_across}│'
+                rows[4] += f'{one_mid}│'
+                rows[5] += f'{two_across}│'
+            elif self.end2_pips == 4:
+                rows[3] += f'{two_across}│'
+                rows[4] += f'{blank_across}│'
+                rows[5] += f'{two_across}│'
+            elif self.end2_pips == 3:
+                rows[3] += f'{one_top}│'
+                rows[4] += f'{one_mid}│'
+                rows[5] += f'{one_btm}│'
+            elif self.end2_pips == 2:
+                rows[3] += f'{one_top}│'
+                rows[4] += f'{blank_across}│'
+                rows[5] += f'{one_btm}│'
+            elif self.end2_pips == 1:
+                rows[3] += f'{blank_across}│'
+                rows[4] += f'{one_mid}│'
+                rows[5] += f'{blank_across}│'
+            elif self.end2_pips == 0:
+                rows[3] += f'{blank_across}│'
+                rows[4] += f'{blank_across}│'
+                rows[5] += f'{blank_across}│'
+        
+        return rows
 
 
 class Boneyard:
@@ -114,7 +345,7 @@ class Player:
         indentation1: str = '    ' * depth
         attributes: str = ""
         attributes += f"\n{indentation1}ai={self.ai}, "
-        attributes += f"\n{indentation1}hand={self.hand})"
+        attributes += f"\n{indentation1}hand={self.hand}"
         return f"\n{indentation0}Player({attributes}\n{indentation0})"
     
     def draw(self, boneyard: Boneyard) -> None:
@@ -206,6 +437,70 @@ class Board:
             attributes += f"\n{indentation1}]"
         return f"\n{indentation0}Board({attributes}\n{indentation0})"
 
+    def initial_trains(self) -> None:
+        """Lets every player add an initial train to their personal branch.
+        
+        AI players automatically calculate an initial train using the most pips;
+        Regular players can choose the dominoes in their initial train.
+        """
+        print(f"\nROUND {self.round_num}: INITIAL TRAINS\n")
+        for branch in self.branches:
+            branch.train.append(self.middle_tile)
+        for i_player, player in enumerate(self.players):
+            # Real player picks their dominoes from options list.
+            if not player.ai:
+                print(f"Middle tile: {self.middle_tile}")
+                # Set initial compare value (which is the round number).
+                compare_value: int = self.round_num
+                # Get list of all dominos with ends matching round num and move them to options buffer.
+                options: list[Domino] = [domino for domino in player.hand if domino.end1_pips == compare_value or domino.end2_pips == compare_value]
+                for domino in options:
+                    player.hand.remove(domino)
+                    if domino.end2_pips == compare_value:
+                        domino.end1_pips, domino.end2_pips = domino.end2_pips, domino.end1_pips
+                print(f"Your hand: {player.hand}")
+                # Continue while options for playing still remain.
+                while options:
+                    # Print list with indicies for picking choice.
+                    print(f"Option number and domino for first domino in train: {[(i_options, domino) for i_options, domino in enumerate(options)]}")
+                    # Get choice (index).
+                    choice: int = int(input(f"Pick which domino to start: {[i_options for i_options in range(len(options))]}\n> "))
+
+                    choice_domino: Domino = options[choice]
+
+                    # Add domino to branch and remove from options buffer.
+                    self.branches[i_player].add_to_train(choice_domino)
+                    options.remove(choice_domino)
+
+                    # Return all dominoes in options buffer to hand.
+                    for domino in options:
+                        player.hand.add(domino)
+
+                    # Get new compare value
+                    if choice_domino.end1_pips == compare_value:
+                        new_compare_value: int = choice_domino.end2_pips
+                    else:
+                        new_compare_value: int = choice_domino.end1_pips
+
+                    # Replace old compare_value
+                    compare_value = new_compare_value
+
+                    # Get new list of options for next domino in train and move from hand to options buffer.
+                    options = [domino for domino in player.hand if domino.end1_pips == compare_value or domino.end2_pips == compare_value]
+                    for domino in options:
+                        player.hand.remove(domino)
+                        if domino.end2_pips == compare_value:
+                            domino.end1_pips, domino.end2_pips = domino.end2_pips, domino.end1_pips
+
+                    # Print train and hand to screen
+                    print(f"Train: {self.branches[i_player].train}")
+                    print(f"Hand: {player.hand}")
+                else:
+                    print("No options\n\n")
+            # AI calculates best initial train
+            else:
+                ...
+
 
 class Game:
     """Handles all game actions."""
@@ -214,6 +509,7 @@ class Game:
         self.ammt_players: int = AMMT_PLAYERS
         self.players: list[Player] = []
         # Create each player, taking amount of AI players into account
+        print(f"\nGAME: START\n")
         ammt_players_real = int(input(f"How many real players? (1-{self.ammt_players})\n> "))
         for _ in range(self.ammt_players):
             if ammt_players_real:
@@ -259,7 +555,7 @@ class Game:
         self._round_init()
         self._update_turn()
         
-        # initial_train()                   # TODO
+        self.board.initial_trains()         # TODO AI
         # show_possible_moves()             # TODO
         # if no_possible_moves              # TODO
         #    draw_tile()                    # TODO
@@ -294,7 +590,7 @@ def main():
     """Runs the game."""
     game = Game()
     game.gameloop()
-    print(game)
+    # print(game)
     
 
 if __name__ == '__main__':
